56b49d551c70a0a32873ec08998d5cdf76569883
gllmar <gllm@artificiel.org>
2017-01-17 11:19:17 +0000

    ~services

--- pix2light/scripts/refreshServices.sh
+++ pix2light/scripts/refreshServices.sh
@@ -6,7 +6,7 @@ echo ">>>> STARTING" $0 "on" $HOSTNAME ">>>>"
 echo "Script : git pull";
 cd /home/artificiel/src/of/app/pix2light && git pull ;
 echo "Script : ln services et timer vers /etc/systemd/system";
-sudo ln -s /home/artificiel/src/of/app/pix2light/pix2light/systemd/*  /etc/systemd/system/ ;
+sudo ln -s /home/artificiel/src/of/apps/pix2light/pix2light/systemd/*  /etc/systemd/system/ ;
 echo "Script :daemon-reload";
 sudo systemctl daemon-reload ;
 echo "Script : refreshServices finish on" $HOSTNAME;
